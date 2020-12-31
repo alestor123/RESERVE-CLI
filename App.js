d68822289a40016beac3c1b17b8913c2eb657cc8
alestor123 <alestoraldous@gmail.com>
2020-12-31 18:19:55 +0530

    added name key

--- App.js
+++ App.js
@@ -5,4 +5,6 @@ fs = require('fs'),
 tmp = require('tmp'),
 conf = require('conf'),
 options = require('minimist'),
+key = options.k || options.key,
+name = options.name || options.n ,
 npm = require('npm-registry-client');
