2b9c3adc282eb2beb93352d7f74251b8f571a400
alestor123 <alestoraldous@gmail.com>
2020-12-31 18:14:37 +0530

    modules

--- App.js
+++ App.js
@@ -1,1 +1,8 @@
 #!/usr/bin/env node
+
+var path = require('path'),
+fs = require('fs'),
+tmp = require('tmp'),
+conf = require('conf'),
+options = require('minimist'),
+npm = require('npm-registry-client');
