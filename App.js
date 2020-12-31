199e7bea9ce13e3b4ea44954316dec5d2c5522e4
alestor123 <alestoraldous@gmail.com>
2020-12-31 18:58:58 +0530

    added main code

--- App.js
+++ App.js
@@ -2,10 +2,26 @@
 
 var path = require('path'),
 fs = require('fs'),
+{ exec } = require('child_process'),
 tmp = require('tmp'),
-conf = require('conf'),
-options = require('minimist'),
+options = require('minimist')(process.argv.slice(2)),
 name = options.name || options.n;
 
-
 // readme 
+var readme = `# ${name}
+
+> WIP`,
+
+pckW = {
+        "name": name,
+        "version": "0.0.0",
+        "description": "Reserved [WIP]",
+      
+}
+tmp.dir((err, path) => {
+if(err) throw err
+fs.writeFile(path + "/package.json",JSON.stringify(pckW),(err) => {if(err) throw err})
+fs.writeFile(path + "/README.md",readme,(err) => {if(err) throw err})
+exec(`npm publish ${path}`,(err,stdout) => { if(err) throw err
+console.log(stdout)})})
+    
